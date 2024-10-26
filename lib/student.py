9fd91b236786798f3d24b4a06aef887490ad0e20
Melvin jerry <+>
2024-10-26 16:52:16 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge=[]
+
     
-    def learn(self):
-        pass
+    def learn(self, information: str):
+         self.knowledge.append(information)  # Add the information to the list
+    pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
-
 from user import User
-
-import random
+import random  # Ensure the random module is imported
 
 class Teacher(User):
+    def __init__(self, first_name, last_name) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        '''Returns a random knowledge string from the knowledge list.'''
+        random_index = random.randint(0, len(self.knowledge) - 1)  # Get a random index
+        return self.knowledge[random_index]  # Return the element at that index
